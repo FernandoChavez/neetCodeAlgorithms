61852b3e5f3c2f7bd6ed78e1bce0875eeca8e8a4
FuerzaDON <ingluisfernandochavez@hotmail.com>
2025-05-09 22:12:29 -0600

    LongestConsecutiveSequence algorithme completed

--- src/main/java/org/fuerzadon/com/arraysAndHashing/LongestConsecutiveSequence/main.java
+++ src/main/java/org/fuerzadon/com/arraysAndHashing/LongestConsecutiveSequence/main.java
@@ -1,7 +1,20 @@
 package org.fuerzadon.com.arraysAndHashing.LongestConsecutiveSequence;
 
+import java.util.Arrays;
+
 public class main {
     public static void main(String[] args) {
+        int nums[] = {2,20,4,10,3,4,5} , nums2[] = {0,3,2,5,4,6,1,11}, nums3[] = {1,1,2,1,1,1 },
+                nums4[] = {5,1,66,0,65,67,3}, nums5[] = {0,3,5,7,9,10}, nums6[] = {0,3,5,7,9,10, 11},
+                nums7[] = { 0,3,5,7,9 };
+
+        System.out.println(methods.longestConsecutive2(nums));
+        System.out.println(methods.longestConsecutive2(nums2));
+        System.out.println(methods.longestConsecutive2(nums3));
+        System.out.println(methods.longestConsecutive2(nums4));
+        System.out.println(methods.longestConsecutive2(nums5));
+        System.out.println(methods.longestConsecutive2(nums6));
+        System.out.println(methods.longestConsecutive2(nums7));
 
     }
 }
--- src/main/java/org/fuerzadon/com/arraysAndHashing/LongestConsecutiveSequence/methods.java
+++ src/main/java/org/fuerzadon/com/arraysAndHashing/LongestConsecutiveSequence/methods.java
@@ -1,4 +1,71 @@
 package org.fuerzadon.com.arraysAndHashing.LongestConsecutiveSequence;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class methods {
+
+    public static int longestConsecutive(int[] nums){
+
+        if(nums.length==0){
+            return 0;
+        }
+
+        List<Integer> list = Arrays.stream(nums)
+                .boxed()
+                .sorted()
+                .collect(Collectors.toList());
+
+
+        if(list.size()==1){
+            return 1;
+        }
+
+        int consecutive = 0, maxC=0;
+        for(int i=0; i<list.size()-1; i++){
+            if((list.get(i)+1 == list.get(i+1))){
+                if(consecutive==0){
+                    consecutive+=2;
+                }else{
+                    consecutive+=1;
+                }
+            }else{
+                if(consecutive>0){
+                   maxC=Math.max(maxC, consecutive) ;
+                   consecutive = 0;
+                }
+            }
+        }
+
+        if(consecutive>=0){
+            maxC=Math.max(maxC, consecutive) ;
+        }
+
+        return maxC;
+    }
+
+    public static int longestConsecutive2(int[] nums){
+        Set<Integer> numSet = new HashSet<>();
+        for(int num : nums){
+            numSet.add(num);
+        }
+        int longest = 0;
+
+        for(int num : numSet){
+            if(!numSet.contains(num-1)){
+                int length = 1;
+
+                while(numSet.contains(num + length)){
+                    length++;
+                }
+                longest = Math.max(longest, length);
+
+            }
+        }
+        return longest;
+    }
+
 }
